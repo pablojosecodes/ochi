8da21851bd39303b37b3cbdb546dcf7c56f66fdf
pablojosecodes <pablojrhansen@gmail.com>
2024-02-07 16:39:30 +0100

    init

--- src/types/Card.ts
+++
@@ -1,6 +0,0 @@
-interface Card {
-  term: string;
-  content: string;
-}
-
-export default Card;
--- src/types/Settings.ts
+++
@@ -1,9 +0,0 @@
-interface Settings {
-  useDefaultSaveLocation: boolean;
-  cardTag: string;
-  deckNamingOption: string;
-  defaultSaveLocation: string;
-  hideTagInPreview: boolean;
-}
-
-export default Settings;
