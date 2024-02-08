4dd8221f2d8da6202f78b910a23f822af6df56ac
pablojosecodes <pablojrhansen@gmail.com>
2024-02-08 19:25:32 +0100

    Format lines more efficiently

--- main.ts
+++ main.ts
@@ -133,10 +133,10 @@ export default class ObsidianToMochiPlugin extends Plugin {
 				// Check if the line has content
 				if (line.trim().length > 0) {
 					// If yes, insert a new line after the current line and then the comment
-					editor.replaceRange("\n<!---->\n", { line: cursor.line + 1, ch: 0 });
+					editor.replaceRange("<!---->", { line: cursor.line + 1, ch: 0 });
 				} else {
 					// If the line is empty, insert the comment at the current cursor position
-					editor.replaceRange("<!---->\n", cursor);
+					editor.replaceRange("<!---->", cursor);
 				}
 			}
 		});
